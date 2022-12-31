81be2170aa42301b3dcc41b1b19fe6add1da6d51
iqbalrizan <iqbalrizan@gmail.com>
2022-12-31 13:11:05 +0700

    initial-commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [temperatureValue, setTemperatureValue] = useState(20);
+  const [color, setColor] = useState("neutral");
+  const [background, setBackground] = useState("bgnetral");
+
+  const increaseTemperature = () => {
+    const nextTemperature = temperatureValue + 1;
+    if (nextTemperature > 29) {
+      setColor("hot29");
+      setBackground("bghottest");
+    } else if (nextTemperature >= 27 && nextTemperature < 29) {
+      setColor("hot27");
+    } else if (nextTemperature >= 25 && nextTemperature < 27) {
+      setColor("hot25");
+      setBackground("bghot");
+    } else if (nextTemperature >= 23 && nextTemperature < 25) {
+      setColor("hot23");
+    } else if (nextTemperature >= 21 && nextTemperature < 23) {
+      setColor("hot21");
+    } else if (nextTemperature >= 19 && nextTemperature < 21) {
+      setColor("cold19");
+      setBackground("bgnetral");
+    } else if (nextTemperature >= 17 && nextTemperature < 19) {
+      setColor("cold17");
+    } else if (nextTemperature >= 15 && nextTemperature < 17) {
+      setColor("cold15");
+      setBackground("bgcold");
+    } else if (nextTemperature >= 13 && nextTemperature < 15) {
+      setColor("cold13");
+    } else if (nextTemperature >= 11 && nextTemperature < 13) {
+      setColor("cold11");
+      setBackground("bgcoldest");
+    }
+    setTemperatureValue(nextTemperature);
+  };
+
+  const decreaseTemperature = () => {
+    const nextTemperature = temperatureValue - 1;
+    if (nextTemperature > 29) {
+      setColor("hot29");
+      setBackground("bghottest");
+    } else if (nextTemperature >= 27 && nextTemperature < 29) {
+      setColor("hot27");
+    } else if (nextTemperature >= 25 && nextTemperature < 27) {
+      setColor("hot25");
+      setBackground("bghot");
+    } else if (nextTemperature >= 23 && nextTemperature < 25) {
+      setColor("hot23");
+    } else if (nextTemperature >= 21 && nextTemperature < 23) {
+      setColor("hot21");
+    } else if (nextTemperature >= 19 && nextTemperature < 21) {
+      setColor("cold19");
+      setBackground("bgnetral");
+    } else if (nextTemperature >= 17 && nextTemperature < 19) {
+      setColor("cold17");
+    } else if (nextTemperature >= 15 && nextTemperature < 17) {
+      setColor("cold15");
+      setBackground("bgcold");
+    } else if (nextTemperature >= 13 && nextTemperature < 15) {
+      setColor("cold13");
+    } else if (nextTemperature >= 11 && nextTemperature < 13) {
+      setColor("cold11");
+      setBackground("bgcoldest");
+    }
+    setTemperatureValue(nextTemperature);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className={background}>
+      <div className="App">
+        <h1>TEMPERATURE APP</h1>
+        <div className="app-container">
+          <div className="temperature-display-container">
+            <div className={`temperature-display ${color}`}>{temperatureValue}&deg;C</div>
+          </div>
+          <h3>SHARP</h3>
+          <div className="button-container">
+            <button onClick={increaseTemperature}>+</button>
+            <button onClick={decreaseTemperature}>-</button>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
